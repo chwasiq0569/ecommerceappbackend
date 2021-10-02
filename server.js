4bc44836883bc863d24fe81061ce0135cd071181
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-10-03 00:39:07 +0500

    Implemented Router

--- server.js
+++ server.js
@@ -1,41 +1,61 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 const app = express();
+app.use(express.json());
+dotenv.config();
 
-const db =
-  "mongodb+srv://admin:admin@cluster0.e6iva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const db = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.e6iva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose
   .connect(db, {
-    useNewURLParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
   })
   .then(() => {
     console.log("DB CONNECTED");
   })
   .catch((err) => console.log("ERROR", err));
 
+app.use("/api/v1", userRoute);
+
+// app.get("/", (req, res) => {
+//   const tourSchema = mongoose.Schema({
+//     name: {
+//       type: String,
+//       required: [true, "A Tour must have a name."],
+//       unique: true,
+//     },
+//     rating: {
+//       type: Number,
+//       default: 4.5,
+//     },
+//     price: {
+//       type: Number,
+//       required: [true, "A Tour must have a price."],
+//     },
+//   });
+
+//   const Tour = mongoose.model("Tour", tourSchema);
+
+//   const testTour = new Tour({
+//     name: "The Forest Hiker",
+//     rating: 4.7,
+//     price: 497,
+//   });
+
+//   testTour
+//     .save()
+//     .then((doc) => console.log("doc", doc))
+//     .catch((err) => console.log("Error", err));
+
+//   res.send("SEND");
+// });
+
 const PORT = process.env.PORT || 3000;
-
-const tourSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A Tour must have a name."],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, "A Tour must have a price."],
-  },
-});
-
-const Tour = mongoose.model("Tour", tourSchema);
 
 app.listen(PORT, () => {
   console.log(`listening at PORT ${PORT}`);
