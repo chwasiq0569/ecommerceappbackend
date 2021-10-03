e9c53630c8111a649ade33ee1b0b1e18b9cc180a
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-10-03 13:00:14 +0500

    Implemented JWT

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -42,7 +43,16 @@ router.post("/login", async (req, res) => {
       if (userPassword === req.body.password) {
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        const accessToken = jwt.sign(
+          {
+            id: user?.id,
+            isAdmin: user?.isAdmin,
+          },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "3d" }
+        );
+
+        res.status(200).json({ ...others, accessToken });
       } else {
         res
           .status(401)
