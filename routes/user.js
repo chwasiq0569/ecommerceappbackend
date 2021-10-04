3ceebe2545a0bd4491ff2d3461a27fcdd75d77ac
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-10-05 00:03:51 +0500

    Implemented Stats API

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,21 @@ const User = require("../models/User");
 
 const router = require("express").Router();
 
+router.get("/userstats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      { $project: { month: { $month: "$createdAt" } } },
+      { $group: { _id: "$month", total: { $sum: 1 } } },
+    ]);
+
+    res.status(200).json(data);
+  } catch (err) {}
+});
+
 router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
